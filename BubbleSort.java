0b7c16eedc593ed740a648e4c9b101a81db2dc0e
Shriansh Jain <39981157+shrianshjain@users.noreply.github.com>
2019-02-16 16:24:56 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,16 @@
+public class BubbleSort {	
+	public static void bubbleSort(int[] arr){
+      for(int i=0;i<arr.length;i++)
+      {
+        for(int j=0;j<arr.length-i-1;j++)
+        {
+          if(arr[j]>arr[j+1])
+          {
+            int swap = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = swap;
+          }
+        }
+      }
+	}
+}
